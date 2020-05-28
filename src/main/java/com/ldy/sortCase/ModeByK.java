39517baaaae24e5a1de984edd4fd55b7bbc76a26
liudongyang3 <liudongyang8@jd.com>
2020-05-28 09:02:12 +0800

    tree

--- src/main/java/com/ldy/sortCase/ModeByK.java
+++ src/main/java/com/ldy/sortCase/ModeByK.java
@@ -1,12 +1,10 @@
 package com.ldy.sortCase;
 
-import java.util.HashMap;
-import java.util.Map;
-
 /**
  * Create by liudongyang8 on 2020/5/27
+ * 同余原理
  */
-public class IntBy {
+public class ModeByK {
     public static void main(String[] args) {
         int[] A = new int[]{4, 5, 0, -2, -3, 1};
         int k = 5;
@@ -14,16 +12,21 @@ public class IntBy {
     }
 
     public static int subarraysDivByK(int[] A, int K) {
-        Map<Integer, Integer> record = new HashMap<>();
-        record.put(0, 1);
+//        Map<Integer, Integer> record = new HashMap<>();
+//        record.put(0, 1);
+        int[] record = new int[5];
+        record[0] = 1;
+
         int sum = 0, ans = 0;
         for (int elem : A) {
             sum += elem;
             // 注意 Java 取模的特殊性，当被除数为负数时取模结果为负数，需要纠正
             int modulus = (sum % K + K) % K;
-            int same = record.getOrDefault(modulus, 0);
+//            int same = record.getOrDefault(modulus, 0);
+            int same = record[modulus];
             ans += same;
-            record.put(modulus, same + 1);
+            record[modulus] = same + 1;
+//            record.put(modulus, same + 1);
         }
         return ans;
     }

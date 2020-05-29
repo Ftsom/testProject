cd2c29fc08396e934a36a3a951783bc3d8fce07d
liudongyang3 <liudongyang8@jd.com>
2020-05-29 09:35:23 +0800

    tree

--- src/main/java/com/ldy/sortCase/IntHuiwen.java
+++ src/main/java/com/ldy/sortCase/IntHuiwen.java
@@ -5,24 +5,31 @@ package com.ldy.sortCase;
  */
 public class IntHuiwen {
     public static void main(String[] args) {
-        int a = 1221;
+        int a = 12221;
         System.out.println(isPalindrome(a));
     }
 
     public static boolean isPalindrome(int x) {
-        int div = 1;
-        while (x / div >= 10) {
-            div *= 10;
+//        int div = 1;
+//        while (x / div >= 10) {
+//            div *= 10;
+//        }
+//        while (x > 0) {
+//            int left = x / div;
+//            int right = x % 10;
+//            if (left != right) {
+//                return false;
+//            }
+//            x = (x % div) / 10;
+//            div /= 100;
+//        }
+//        return true;
+
+        int reverseNum = 0;
+        while (x > reverseNum) {
+            reverseNum = reverseNum * 10 + x % 10;
+            x /= 10;
         }
-        while (x > 0) {
-            int left = x / div;
-            int right = x % 10;
-            if (left != right) {
-                return false;
-            }
-            x = (x % div) / 10;
-            div /= 100;
-        }
-        return true;
+        return reverseNum == x || reverseNum / 10 == x;
     }
 }

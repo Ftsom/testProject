54ded535578d896cb00ea2e7b776b37d25491d11
liudongyang3 <liudongyang8@jd.com>
2020-06-02 17:20:50 +0800

    tree

---
+++ src/main/java/com/ldy/sortCase/IntegerTest.java
@@ -0,0 +1,13 @@
+package com.ldy.sortCase;
+
+/**
+ * Create by liudongyang8 on 2020/6/2
+ */
+public class IntegerTest {
+    public static void main(String[] args) {
+        Integer a = 1000;
+        Integer b = 1000;
+
+        System.out.println(a ==b);
+    }
+}
